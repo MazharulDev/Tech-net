d8ed55f1fb0b0a5f39d380dbf7b5862bac258a14
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-07-09 07:29:03 +0600

    simple counter app created of redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+
 function App() {
 
+  const {count} =useAppSelector((state)=>state.counter)
+  const dispatch=useAppDispatch();
+
   return (
-    <h1>Tech net</h1>
+    <div className="mt-4">
+      <div className="flex justify-center items-center gap-5">
+        <button onClick={()=>dispatch(increment())} className="p-4 border rounded-md border-green-500 hover:bg-green-400">increment</button>
+        <button onClick={()=>dispatch(incrementByAmount(5))} className="p-4 border rounded-md border-green-500 hover:bg-green-400">increment by value</button>
+        <p>{count}</p>
+        <button onClick={()=>dispatch(decrement())} className="p-4 border rounded-md border-red-500 hover:bg-red-400">decrement</button>
+      </div>
+    </div>
   )
 }
 
